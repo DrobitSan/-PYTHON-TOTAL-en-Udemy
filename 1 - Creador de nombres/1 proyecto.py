e169541b7dd11286c9ead1cc10f5b56c545e9d1f
Serhiy Drobit <58827424+DrobitSan@users.noreply.github.com>
2023-04-20 02:25:43 +0200

    Update 1 proyecto.py

--- 1 - Creador de nombres/1 proyecto.py
+++ 1 - Creador de nombres/1 proyecto.py
@@ -1,24 +1,5 @@
 #Crear un programa que sume dos nombres ingreasados con salto de linea y con comillas
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 print("Tu marca de cerveza es:\n" + input('Año de tu nacimiento: ') + "'s " + input('Tu género de música preferido: '))
+
